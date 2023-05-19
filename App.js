f15eb556730618f173df646d64c94195bdb9b439
AungKo2019 <highflier.ako1991@gmail.com>
2023-05-19 20:31:42 +0630

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button,Alert, View } from 'react-native';
 
 export default function App() {
+
+  const create3ButtonAlert=()=>{
+    Alert.alert(
+      "Testing Alert",
+      "Your are am amazing Dev",
+      [
+        {
+          text:"Ask Me Later",
+          onPress:()=>console.log("Ask Me Later")
+        },
+        {
+          text:"Yeah",
+          onPress:()=>console.log("Yeah"),
+          style:'default'
+        },
+        {
+          text:"Nope",
+          onPress:()=>console.log("Nope"),
+          style:'cancel'
+        }
+      ],
+      {cancelable:false}
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Alert 3 Button" onPress={create3ButtonAlert}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'space-around',
+    alignItems:'center'
+  }
+}
+)
